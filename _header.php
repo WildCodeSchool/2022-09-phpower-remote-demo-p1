fa49f48db8bedb9f01894fd65bfb24c326fb647e
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-09-28 14:30:13 +0200

    header step1

---
+++ _header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <h1>Logo</h1>
+        <ul>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+        </ul>
+    </nav>
+</header>
